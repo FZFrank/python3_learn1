13857ea0b2fc1c3030da99dd8c2e339f37c4763f
lilianplayer <qwebnm173@163.com>
2018-06-03 01:44:52 +0800

    1

--- .idea/学习练习/笔记.py
+++ .idea/学习练习/笔记.py
@@ -9,3 +9,4 @@
 #format方法:'{0},{1},{2}'.format('x','y','z')等价于'{a},{b},{c}'.format(a='x',b='y',c='z') 输出为'x,y,z'
 #字符串格式化 : '{0:1f}{1}'.format(27.658,'GB')输出'27.7GB','%c %c' % (97,98) 输出 'a b'+
 #函数 def test(*a,b=1,c),a为收集参数，参数个数无限多；b为默认参数，默认赋值为1；c为普通参数，不传入则会报错
+#thunder://QUFtYWduZXQ6P3h0PXVybjpidGloOjk5ODIxQkZBRDJFOTdBQTAwRkUxOTIwMzEwRUU1MEVENDE3NUMyOTUmZG49JUUzJTgwJThBJUU2JUFEJUE2JUU2JTlFJTk3JUU1JUE0JTk2JUU0JUJDJUEwJUUzJTgwJThCJUU2JTlDJUFBJUU1JTg4JUEwJUU4JThBJTgyJUU1JThGJUIwJUU2JUI5JUJFJUU2JUI4JTg1JUU2JTk5JUIwJUU3JTg5JTg4Wlo=/
